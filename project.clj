33775d228e0a0b25e50561e7c6d4e042949bdbca
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-09-30 17:36:11 -0300

    main component project

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject intception.com/components "0.1.0"
-  :description "FIXME: write description"
+  :description "Reusable services implementation following the components pattern"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [intception.com.components/lifecycle "0.1.0"]
+                 [intception.com.components/jetty "0.1.0"]
+                 [intception.com.components/datomic "0.1.0"]]
+  :plugins [[lein-sub "0.2.4"]
+            [codox "0.8.5"]]
+  :sub ["lifecycle"
+        "jetty"
+        "datomic"])
