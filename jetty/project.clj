e9ab5cdccc18456b5efeffde42a8ccf51ca03815
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-09-30 17:36:29 -0300

    jetty component

--- jetty/project.clj
+++ jetty/project.clj
@@ -1,7 +1,8 @@
-(defproject intception.com/components/jetty "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject intception.com.components/jetty "0.1.0"
+  :description "Jetty Component"
+  :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [intception.com/components "0.1.0"]])
+                 [intception.com.components/lifecycle "0.1.0"]
+                 [ring/ring-jetty-adapter "1.3.1"]])
--- jetty/src/components/jetty/service.clj
+++ jetty/src/components/jetty/service.clj
@@ -1,6 +1,22 @@
-(ns jetty.core)
+(ns components.jetty.service
+  (require [ring.adapter.jetty :as jetty])
+  (use components.lifecycle.protocol))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord JettyWeb [state routes port]
+  Lifecycle
+  (stop [this system]
+    (when (:jetty @state)
+      (.stop (:jetty @state))))
+  (start [this system]
+    (stop this system)
+    (swap! state
+           assoc :jetty
+           (jetty/run-jetty (if (fn? routes)
+                              (routes system)
+                              routes)
+                            {:join? false :port port}))))
+
+(defn make
+  "Creates a jetty web server component"
+ [{:keys [routes port]}]
+ (->JettyWeb (atom {}) routes port))
