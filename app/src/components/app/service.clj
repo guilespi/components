a576b50772825bbd4033fbdda47d6fdb3293ee3a
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-10-02 16:46:23 -0300

    new service protocol for handler acquiring

--- app/src/components/app/service.clj
+++ app/src/components/app/service.clj
@@ -14,3 +14,15 @@
   "Creates an app component component"
  [& components]
  (->App components))
+
+(defn ->components
+  [app service]
+  (filter #(re-find (re-pattern (format "^components\\.%s\\.service\\.[^.]+$"
+                                        (name service)))
+                    ((comp (memfn getName) class) %))
+          (:components app)))
+
+(defn ->handler
+  [app service]
+  (when-let [s (first (get-component app service))]
+    (handler s)))
--- lifecycle/src/components/lifecycle/protocol.clj
+++ lifecycle/src/components/lifecycle/protocol.clj
@@ -3,3 +3,6 @@
 (defprotocol Lifecycle
   (start [this] [this system] "Start all life")
   (stop [this] [this system] "End all life"))
+
+(defprotocol Service
+  (handler [this] "Retrieves service handler"))
