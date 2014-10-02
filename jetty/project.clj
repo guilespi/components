ed35d941960352e6bf518a163696925c2310d535
Alexis Rodríguez <arodrig@fing.edu.uy>
2014-10-02 12:37:53 -0300

    version bumps

--- jetty/project.clj
+++ jetty/project.clj
@@ -1,8 +1,8 @@
-(defproject com.intception.components/jetty "0.1.1"
+(defproject com.intception.components/jetty "0.1.2"
   :description "Jetty Component"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.1"]
+                 [com.intception.components/lifecycle "0.1.2"]
                  [ring/ring-jetty-adapter "1.3.1"]])
--- lifecycle/project.clj
+++ lifecycle/project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception.components/lifecycle "0.1.1"
+(defproject com.intception.components/lifecycle "0.1.2"
   :description "LifeCycle Components Protocol"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
