4c822b97c58f3e6e25ee50910dfb072e2fc825b7
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-10-02 16:52:56 -0300

    bump components versions

--- project.clj
+++ project.clj
@@ -4,16 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.2"]
-                 [com.intception.components/jetty "0.1.2"]
-                 [com.intception.components/datomic "0.1.2"]
-                 [com.intception.components/app "0.1.2"]
-                 [com.intception.components/redis "0.1.2"]
-                 [com.intception.components/metrics "0.1.2"]]
+                 [com.intception.components/lifecycle "0.1.3"]
+                 [com.intception.components/jetty "0.1.3"]
+                 [com.intception.components/datomic "0.1.3"]
+                 [com.intception.components/app "0.1.3"]
+                 [com.intception.components/redis "0.1.3"]
+                 [com.intception.components/metrics "0.1.3"]]
   :plugins [[lein-sub "0.2.4"]
             [codox "0.8.5"]]
   :sub ["lifecycle"
         "jetty"
         "datomic"
         "app"
-        "redis"])
+        "redis"
+        "metrics"])
