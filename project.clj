ce723fcebebeeccf7e09178657d04a732364597d
Guillermo Winkler <guillermo.winkler@gmail.com>
2015-01-12 15:20:51 -0200

    bump up version to 0.1.6

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject com.intception/components "0.1.5"
+(defproject com.intception/components "0.1.6"
   :description "Reusable services implementation following the components pattern"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.5"]
-                 [com.intception.components/jetty "0.1.5"]
-                 [com.intception.components/datomic "0.1.5"]
-                 [com.intception.components/app "0.1.5"]
-                 [com.intception.components/redis "0.1.5"]
-                 [com.intception.components/metrics "0.1.5"]
-                 [com.intception.components/time-series "0.1.5"]]
+                 [com.intception.components/lifecycle "0.1.6"]
+                 [com.intception.components/jetty "0.1.6"]
+                 [com.intception.components/datomic "0.1.6"]
+                 [com.intception.components/app "0.1.6"]
+                 [com.intception.components/redis "0.1.6"]
+                 [com.intception.components/metrics "0.1.6"]
+                 [com.intception.components/time-series "0.1.6"]]
   :plugins [[lein-sub "0.2.4"]
             [codox "0.8.5"]]
   :sub ["lifecycle"
--- redis/project.clj
+++ redis/project.clj
@@ -1,8 +1,8 @@
-(defproject com.intception.components/redis "0.1.5"
+(defproject com.intception.components/redis "0.1.6"
   :description "Redis Component"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.5"]
+                 [com.intception.components/lifecycle "0.1.6"]
                  [com.taoensso/carmine "2.7.0"]])
