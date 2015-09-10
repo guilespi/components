847f2f7443cbdb810588dae6bf4715f8dd0f4f59
Guillermo Winkler <guillermo.winkler@gmail.com>
2015-09-10 11:26:51 -0300

    Add parameter request-header-size to jetty component to avoid HTTP 413 on large req.
    fixes T440

--- jetty/project.clj
+++ jetty/project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception.components/jetty "0.1.6"
+(defproject com.intception.components/jetty "0.1.7"
   :description "Jetty Component"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
--- jetty/src/components/jetty/service.clj
+++ jetty/src/components/jetty/service.clj
@@ -2,7 +2,7 @@
   (require [ring.adapter.jetty :as jetty])
   (use components.lifecycle.protocol))
 
-(defrecord JettyWeb [state routes port]
+(defrecord JettyWeb [state routes port request-header-size]
   Lifecycle
   (stop [this system]
     (when (:jetty @state)
@@ -14,7 +14,12 @@
            (jetty/run-jetty (if (fn? routes)
                               (routes system)
                               routes)
-                            {:join? false :port port})))
+                            {:join? false
+                             :port port
+                             :configurator (fn [jetty]
+                                             (when request-header-size
+                                               (doseq [connector (.getConnectors jetty)]
+                                                 (.setRequestHeaderSize connector request-header-size))))})))
 
   Service
   (handler [_]
@@ -22,5 +27,5 @@
 
 (defn make
   "Creates a jetty web server component"
- [{:keys [routes port]}]
- (->JettyWeb (atom {}) routes port))
+ [{:keys [routes port request-header-size]}]
+ (->JettyWeb (atom {}) routes port request-header-size))
