f0ea14094ab3049b0791a9844180db651a39207f
Guillermo Winkler <guillermo.winkler@gmail.com>
2016-09-26 21:31:51 -0300

    Add SSL support to jetty component

--- jetty/src/components/jetty/service.clj
+++ jetty/src/components/jetty/service.clj
@@ -2,24 +2,32 @@
   (require [ring.adapter.jetty :as jetty])
   (use components.lifecycle.protocol))
 
-(defrecord JettyWeb [state routes port request-header-size]
+(defrecord JettyWeb [state config]
   Lifecycle
   (stop [this system]
     (when (:jetty @state)
       (.stop (:jetty @state))))
   (start [this system]
-    (stop this system)
-    (swap! state
-           assoc :jetty
-           (jetty/run-jetty (if (fn? routes)
-                              (routes system)
-                              routes)
-                            {:join? false
-                             :port port
-                             :configurator (fn [jetty]
-                                             (when request-header-size
-                                               (doseq [connector (.getConnectors jetty)]
-                                                 (.setRequestHeaderSize connector request-header-size))))})))
+    (let [{:keys [routes port request-header-size
+                  use-ssl ssl-port ssl-keystore
+                  ssl-keystore-password]} config]
+      (stop this system)
+      (swap! state
+             assoc :jetty
+             (jetty/run-jetty (if (fn? routes)
+                                (routes system)
+                                routes)
+                              (merge {:join? false
+                                      :port port
+                                      :configurator (fn [jetty]
+                                                      (when request-header-size
+                                                        (doseq [connector (.getConnectors jetty)]
+                                                          (.setRequestHeaderSize connector request-header-size))))}
+                                     (when use-ssl
+                                       {:ssl? true
+                                        :ssl-port (or ssl-port 443)
+                                        :keystore ssl-keystore
+                                        :key-password ssl-keystore-password}))))))
 
   Service
   (handler [_]
@@ -27,5 +35,5 @@
 
 (defn make
   "Creates a jetty web server component"
- [{:keys [routes port request-header-size]}]
- (->JettyWeb (atom {}) routes port request-header-size))
+ [config]
+ (->JettyWeb (atom {}) config))
