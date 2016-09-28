72d058ebaee87a698dffd992e4154156f8328461
Guillermo Winkler <guillermo.winkler@gmail.com>
2016-09-28 11:59:26 -0300

    BoneCP jdbc connection pooling support for time-series-storage on Postgres, 0.1.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception/components "0.1.18"
+(defproject com.intception/components "0.1.19"
   :description "Reusable services implementation following the components pattern"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
@@ -10,7 +10,7 @@
                  [com.intception.components/app "0.1.6"]
                  [com.intception.components/redis "0.1.7"]
                  [com.intception.components/metrics "0.1.10"]
-                 [com.intception.components/time-series "0.1.11"]
+                 [com.intception.components/time-series "0.1.12"]
                  [com.intception.components/blob-storage "0.1.7"]]
   :plugins [[lein-sub "0.2.4"]
             [codox "0.8.5"]]
--- time-series/src/components/time_series/service.clj
+++ time-series/src/components/time_series/service.clj
@@ -3,7 +3,8 @@
             [time-series-storage.postgres :as postgres]
             [time-series-storage.sql-server :as sql-server])
   (:import [time_series_storage.postgres Postgres]
-           [time_series_storage.sql_server SqlServer])
+           [time_series_storage.sql_server SqlServer]
+           [com.jolbox.bonecp BoneCPDataSource])
   (use components.lifecycle.protocol))
 
 
@@ -34,9 +35,33 @@
     ))
 
 
+(defn make-pool
+  [config]
+  (let [partitions (or (get-in config [:pool :partitions]) 3)
+        min-pool (or (get-in config [:pool :min-pool]) 5)
+        max-pool (or (get-in config [:pool :max-pool]) 20)
+        cpds (doto (BoneCPDataSource.)
+               (.setJdbcUrl (str "jdbc:" (:subprotocol config) ":" (:subname config)))
+               (.setUsername (:user config))
+               (.setPassword (:password config))
+               (.setMinConnectionsPerPartition (inc (int (/ min-pool partitions))))
+               (.setMaxConnectionsPerPartition (inc (int (/ max-pool partitions))))
+               (.setPartitionCount partitions)
+               (.setStatisticsEnabled true)
+               ;; Test connections default is 240 minutes
+               (.setIdleConnectionTestPeriodInMinutes (or (get-in config [:pool :test-period]) 240))
+               ;; Default idle is 60 minutes
+               (.setIdleMaxAgeInMinutes (or (get-in config [:pool :max-age]) 60))
+               ;;Query to avoid connections from dying (varies on database)
+               (.setConnectionTestStatement (or (get-in config [:pool :test-statement])
+                                                "/* ping *\\/ SELECT 1")))]
+    {:datasource cpds}))
+
 (defn make
   "Creates a time-series component from a specified type"
  [{:keys [type config]}]
  (condp = type
-   :postgres (Postgres. config)
+   :postgres (Postgres. (if (:pool config)
+                          (make-pool config)
+                          config))
    :sql-server (SqlServer. config)))
