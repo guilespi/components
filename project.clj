fae654084ed5927c9a245300259b2302d9d8599f
Sebastián Moreno <smoreno.uy@gmail.com>
2018-06-27 10:52:52 -0300

    bump blob storage version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception/components "0.1.28"
+(defproject com.intception/components "0.1.29"
   :description "Reusable services implementation following the components pattern"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,7 @@
                  [com.intception.components/redis "0.1.7"]
                  [com.intception.components/metrics "0.1.11"]
                  [com.intception.components/time-series "0.1.14"]
-                 [com.intception.components/blob-storage "0.1.13"]]
+                 [com.intception.components/blob-storage "0.1.14"]]
   :plugins [[lein-sub "0.2.4"]
             [codox "0.8.5"]]
   :sub ["lifecycle"
