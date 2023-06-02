bd914c5d87fb4d10ca80f5182f3731e0052f13c9
Juan Ramirez <jramirez.uy@gmail.com>
2023-06-02 17:54:13 -0300

    redis: support `password` parameter

--- redis/project.clj
+++ redis/project.clj
@@ -1,8 +1,8 @@
-(defproject com.intception.components/redis "0.1.8"
+(defproject com.intception.components/redis "0.1.9"
   :description "Redis Component"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.6"]
-                 [com.taoensso/carmine "2.7.0"]])
+                 [com.intception.components/lifecycle "0.1.7"]
+                 [com.taoensso/carmine "2.15.1"]])
