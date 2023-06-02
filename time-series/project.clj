9d6157d3eec754c11439dddaf4bfdc7b9555cffe
Juan Ramirez <jramirez.uy@gmail.com>
2023-06-02 17:40:24 -0300

    timeseries: fix ping command

--- time-series/project.clj
+++ time-series/project.clj
@@ -1,10 +1,10 @@
-(defproject com.intception.components/time-series "0.1.17"
+(defproject com.intception.components/time-series "0.1.18"
   :description "Time Series Component"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.intception.components/lifecycle "0.1.6"]
+                 [com.intception.components/lifecycle "0.1.7"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]
                  [org.slf4j/slf4j-log4j12 "1.7.7"]
-                 [time-series-storage "0.3.3"]])
+                 [time-series-storage "0.3.10"]])
