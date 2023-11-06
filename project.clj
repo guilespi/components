09a7bb1ff809508d0cd7e28b9d2e85f1fe6affa0
Juan Ramirez <jramirez.uy@gmail.com>
2023-11-06 13:31:18 -0300

    Update blob storage to 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception/components "0.1.39"
+(defproject com.intception/components "0.2.0"
   :description "Reusable services implementation following the components pattern"
   :url "https://github.com/guilespi/components"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,7 @@
                  [com.intception.components/redis "0.1.10"]
                  [com.intception.components/metrics "0.1.11"]
                  [com.intception.components/time-series "0.1.18"]
-                 [com.intception.components/blob-storage "0.1.18"]]
+                 [com.intception.components/blob-storage "0.2.0"]]
   :plugins [[lein-sub "0.2.4"]
             [codox "0.8.5"]]
   :sub ["lifecycle"
